2f0377b5d49023939a547392f8ae644347346aba
CarsonSallis <csallis@g.clemson.edu>
2018-04-09 21:09:16 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,4 +5,5 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 name = MPI.Get_processor_name()
-print ("Hello world from process %s running on host %s out of %s processes" % (rank, name, size))
+print ("Hello world from process %s running on host %s out of %s processes" % 
+       (rank, name, size)) 
